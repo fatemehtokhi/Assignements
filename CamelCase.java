f1af0f2fa5131d55bd3e63232dd624605a161ea7
fatemeh <fatemeh.rt@gmail.com>
2020-06-03 21:44:16 +0200

    update

--- CamelCase.java
+++ CamelCase.java
@@ -1,21 +1,23 @@
 import java.util.*;
 import java.io.*;
-class CamelCase{
-    public static String toCamelCase (String s) {
+
+class CamelCase {
+    public static String toCamelCase(String s) {
         String words[] = {};
-        if(s.contains("_")) 
-            words = s.split("_"); //"Hello-world", "hello_world"
-        else if(s.contains("-"))
-            words = s.split("-"); 
-        for(int i=0;i<=words.length-1;i++){
-            words[i] = words[i].substring(0,1).toUpperCase()+words[i].substring(1,words[i].length());
+        if (s.contains("_"))
+            words = s.split("_"); // "Hello-world", "hello_world"
+        else if (s.contains("-"))
+            words = s.split("-");
+        for (int i = 0; i <= words.length - 1; i++) {
+            words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length());
         }
-         return String.join("", words);
-    } 
+        return String.join("", words);
+    }
+
     public static void main(String[] args) {
         System.out.println("please enter a sentence :");
         Scanner sent = new Scanner(System.in);
         System.out.println(toCamelCase(sent.nextLine()));
-        
+
     }
 }
