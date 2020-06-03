2a61890806e8e27516eba95afc706c7fd271ec93
fatemeh <fatemeh.rt@gmail.com>
2020-06-03 21:19:55 +0200

    camelcase

--- Abbreviation.java
+++ Abbreviation.java
@@ -4,7 +4,7 @@ public class Abbreviation {
     public static String Abbre(String str) {
        String charsArray[] = str.split(""); 
         int count=str.length()-2; 
-       return charsArray[0]+count +charsArray[str.length()-1]; 
+       return charsArray[0] + count + charsArray[str.length()-1]; 
 
     }
     public static void main(String[] args) {
