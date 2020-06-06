f96210fb5bb559194d65d84e7378e84cf5264295
fatemeh <fatemeh.rt@gmail.com>
2020-06-06 13:45:58 +0200

    update

--- CamelCase.java
+++ CamelCase.java
@@ -5,9 +5,11 @@ class CamelCase {
     public static String toCamelCase(String s) {
         String words[] = {};
         if (s.contains("_"))
-            words = s.split("_"); // "Hello-world", "hello_world"
+            words = s.split("_"); 
         else if (s.contains("-"))
             words = s.split("-");
+        else if (s.contains(" "));
+            words = s.split(" ");
         for (int i = 0; i <= words.length - 1; i++) {
             words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length());
         }
