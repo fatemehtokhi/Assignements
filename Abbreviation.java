5ac2a3b090bf5416b28a0719bb16c62a7cac16b6
fatemeh <fatemeh.rt@gmail.com>
2020-06-07 21:31:04 +0200

    update

--- Abbreviation.java
+++ Abbreviation.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Abbreviation {
     public static String Abbre(String str) {
-       String charsArray[] = str.split(""); 
-        int count=str.length()-2; 
-       return charsArray[0] + count + charsArray[str.length()-1]; 
+        String charsArray[] = str.split("");
+        int count = str.length()-2;
+        return charsArray[0] + count + charsArray[str.length()-1];
 
     }
+
     public static void main(String[] args) {
         System.out.println("Enter a sentece:");
         Scanner q = new Scanner(System.in);
